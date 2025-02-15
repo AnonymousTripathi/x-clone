cfc3f0f6aa3a2eabe10344218423059d5ff84ca2
Harsh <tripathiharsh227@gmail.com>
2025-02-15 12:52:56 +0530

    4.prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import postRoutes from "./routes/post.routes.js"
@@ -20,6 +21,7 @@ cloudinary.config({
 
 const app =express();
 const PORT = process.env.PORT || 5000;
+const __dirname=path.resolve();
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -27,6 +29,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes)
+if(process.env.Node_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
